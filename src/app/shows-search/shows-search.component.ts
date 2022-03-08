23d3917f9b0bad4a4118876cd1f49057a01d288f
Stefan Dimitrovski <stefan.dimitrovski_19@hotmail.com>
2022-03-08 12:58:11 +0100

    show search value changes refactor;

--- src/app/shows-search/shows-search.component.ts
+++ src/app/shows-search/shows-search.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, filter, switchMap } from 'rxjs';
 import { Show } from '../domain/show';
 import { ShowsService } from '../shows.service';
 
@@ -8,22 +9,48 @@ import { ShowsService } from '../shows.service';
     templateUrl: './shows-search.component.html',
     styleUrls: ['./shows-search.component.scss'],
 })
-export class ShowsSearchComponent {
+export class ShowsSearchComponent implements OnInit {
     showForm = new FormControl('');
     shows: Show[] = [];
     isLoading = false;
+    errorMsg = '';
+    nothingFound = false;
 
     constructor(private showService: ShowsService) {}
 
-    searchShow() {
-        this.isLoading = !this.isLoading;
-        this.shows = [];
-        this.showService
-            .getShowsBySearchTerm(this.showForm.value)
-            .subscribe((data) => {
-                this.isLoading = !this.isLoading;
-                this.shows = data;
+    ngOnInit(): void {
+        this.showForm.valueChanges
+            .pipe(
+                debounceTime(450),
+                filter((value) => value),
+                distinctUntilChanged(),
+                switchMap((searchTerm) => {
+                    this.isLoading = !this.isLoading;
+                    this.nothingFound = false;
+                    this.shows = [];
+                    return this.showService.getShowsBySearchTerm(searchTerm);
+                })
+            )
+            .subscribe({
+                next: (data) => {
+                    this.isLoading = !this.isLoading;
+                    if (data.length === 0) {
+                        this.nothingFound = true;
+                        this.errorMsg = 'No shows found!';
+                    } else {
+                        this.nothingFound = false;
+                        this.shows = data;
+                    }
+                },
+                error: (error) => {
+                    this.isLoading = !this.isLoading;
+                    this.errorMsg =
+                        'Unable to contact server, please try again later';
+                },
             });
+    }
+
+    searchShow() {
         this.showForm.reset();
     }
 }
