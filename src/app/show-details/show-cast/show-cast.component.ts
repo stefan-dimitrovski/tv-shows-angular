c91193235f923542314a1c87e09a997b180a6b0b
Stefan Dimitrovski <stefan.dimitrovski_19@hotmail.com>
2022-03-09 13:13:24 +0100

    show cast refactor;

--- src/app/show-details/show-cast/show-cast.component.ts
+++ src/app/show-details/show-cast/show-cast.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ShowCast } from 'src/app/domain/show-cast';
 
 @Component({
-  selector: 'app-show-cast',
-  templateUrl: './show-cast.component.html',
-  styleUrls: ['./show-cast.component.scss']
+    selector: 'app-show-cast',
+    templateUrl: './show-cast.component.html',
+    styleUrls: ['./show-cast.component.scss'],
 })
-export class ShowCastComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ShowCastComponent {
+    @Input() cast: ShowCast[] = [];
 }
